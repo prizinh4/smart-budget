63965a04bf1fd24ff153291ae18dbbcffb2969e4
Priscila Andrade <primoraesandrade57@example.com>
2026-03-01 11:06:51 -0300

    feat: create user entity and module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       synchronize: true, 
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
